c963b327f73962bb0324989b6d6b54f037f18b8b
hidemon <yusun@utexas.edu>
2015-10-22 22:11:05 -0500

    Fix bug

--- TextAnalyzer/src/TextAnalyzer.java
+++ TextAnalyzer/src/TextAnalyzer.java
@@ -8,22 +8,29 @@ import org.apache.hadoop.util.*;
 
 public class TextAnalyzer {
 	public static class Map extends MapReduceBase implements
-			Mapper<LongWritable, Text, Text, MapWritable> {
+			Mapper<LongWritable, Text, Text, MyMap> {
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
 
 		public void map(LongWritable key, Text value,
-				OutputCollector<Text, MapWritable> output, Reporter reporter)
+				OutputCollector<Text, MyMap> output, Reporter reporter)
 				throws IOException {
-			//Convert the capital chars to lower chars.		
+			//Convert the capital chars to lower chars.
 			String line = value.toString().toLowerCase();
 			String[] words = line.split("([^a-z0-9])+");
 			String[] contextWords = words.clone(); // Set up the context word array.
 			String[] queryWords = words.clone(); // Set up the query word array.
-			MapWritable map = new MapWritable();
+			Set <String> set = new HashSet<String>();
 			for (int i = 0; i < contextWords.length; i++) {
 				String contextWord = contextWords[i];
+				if(set.contains(contextWord)) {   //The same context word is calculated only once.
+					continue;
+				}
+				set.add(contextWord);
+				Text contextText = new Text(contextWord);
             	boolean isSelf = true;  //In order to prevent it from calculating contextWord itself.
+            	MyMap map = new MyMap();
+            	HashMap <String, Integer> hashMap = new HashMap<String, Integer>();
             	for (int j = 0; j < queryWords.length; j++) {
             		String queryWord = queryWords[j];
             		if (queryWord.equals(contextWord)) {
@@ -31,43 +38,58 @@ public class TextAnalyzer {
             				isSelf = false;
             			    continue;
             			}
-            			map.put(new Text(queryWord), one);
-            			output.collect(new Text(contextWord), map);	
-            		} 
-            		
+            		}
+            		if (!hashMap.containsKey(queryWord)) {       //Outcome map doesn't contain the key 
+						hashMap.put(queryWord, 1);
+					} else {
+						hashMap.put(queryWord, hashMap.get(queryWord) + 1); //key appearing num + 1
+					}
             	}
+            	for (java.util.Map.Entry<String, Integer> entry: hashMap.entrySet()) {
+            		map.put(new Text(entry.getKey()), new IntWritable(entry.getValue()));
+            	}
+            	output.collect(contextText, map);
 			}
 
 		}
 	}
-
+	public static class MyMap extends MapWritable {
+		public String toString() {
+			StringBuilder builder = new StringBuilder();
+			builder.append("\n");
+			for(java.util.Map.Entry<Writable,Writable> entry : this.entrySet()) {
+				builder.append("<" + ((Text)entry.getKey()).toString() + ", " + ((IntWritable)entry.getValue()).get() + ">\n");	
+			}
+			return builder.toString();
+		}
+	}
 	public static class Reduce extends MapReduceBase implements
-			Reducer<Text, MapWritable, Text, MapWritable> {
-		public void reduce(Text key, Iterator<Context> values,
-				OutputCollector<Text, MapWritable> output, Reporter reporter)
+			Reducer<Text, MyMap, Text, MyMap> {
+		public void reduce(Text key, Iterator<MyMap> values,
+				OutputCollector<Text, MyMap> output, Reporter reporter)
 				throws IOException {
-			int sum = 0;
-			MapWritable map = new MapWritable();
+			MyMap map = new MyMap();
 			while (values.hasNext()) {
-				MapWritable map_branch = values.next().get();
-				for (Text key : map_branch.keySet()) {    //Iterate the keyset
-					if (!map.containsKey(key)) {       //Outcome map doesn't contain the key 
-						map.put(key, map_branch.get(key));  
+				MyMap map_branch = values.next();
+				for (Writable k : map_branch.keySet()) {    //Iterate the keyset
+					Text keyOfMap = (Text)k;
+					if (!map.containsKey(keyOfMap)) {       //Outcome map doesn't contain the key 
+						map.put(keyOfMap, map_branch.get(keyOfMap));
 					} else {
-						map.put(key, new IntWritable(map.get(key) + 1)); //key appearing num + 1
+						map.put(keyOfMap, new IntWritable(((IntWritable)map.get(keyOfMap)).get() + ((IntWritable)map_branch.get(keyOfMap)).get())); //key appearing num + 1
 					}
 				}
-				
+
 			}
 			output.collect(key, map);
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
-		JobConf conf = new JobConf(WordCount.class);
+		JobConf conf = new JobConf(TextAnalyzer.class);
 		conf.setJobName("TextAnalyzer");
 		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
+		conf.setOutputValueClass(MyMap.class);
 		conf.setMapperClass(Map.class);
 		conf.setCombinerClass(Reduce.class);
 		conf.setReducerClass(Reduce.class);
