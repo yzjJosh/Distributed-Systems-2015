509052d4517d2102c53a862b874c7553907e00a3
hidemon <yusun@utexas.edu>
2015-11-05 14:22:00 -0600

    FingerTable

---
+++ FinalProject/FusionBackup/src/chord/ChordNode.java
@@ -0,0 +1,12 @@
+package chord;
+
+/**
+ * Provides all methods necessary for a user application. This includes methods
+ * for changing connectivity to the network (create, join, leave) as well as for
+ * working with content (insert, retrieve, remove).
+ * 
+ * @author 	Yu Sun
+ */
+public class ChordNode {
+
+}
