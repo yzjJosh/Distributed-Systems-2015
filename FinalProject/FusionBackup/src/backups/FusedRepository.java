20dd1a54162899547b0694e67155f552e702ebe1
Josh <yangzijiangjosh@gmail.com>
2015-11-05 19:39:42 -0600

    Seperate two projects

--- FinalProject/FusionBackup/src/backups/FusedRepository.java
+++ FinalProject/FusionBackup/src/backups/FusedRepository.java
@@ -6,13 +6,13 @@ package backups;
  *
  */
 public class FusedRepository {
-	public final DoubleMatrix data;
+//	public final DoubleMatrix data;
 	
 	/**
 	 * Initialize this repository and set the maximum node size of this repository.
 	 * @param maxNodeSize The maximum size of each node in this repository
 	 */
 	public FusedRepository(int maxNodeSize){
-		this.maxNodeSize = maxNodeSize;
+	//	this.maxNodeSize = maxNodeSize;
 	}
 }
