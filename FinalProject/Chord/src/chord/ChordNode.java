b20514a8bb51888b1325f53cc9d0adde03469a28
hidemon <yusun@utexas.edu>
2015-11-06 17:17:59 -0600

    ChordThread

--- FinalProject/Chord/src/chord/ChordNode.java
+++ FinalProject/Chord/src/chord/ChordNode.java
@@ -1,5 +1,9 @@
 package chord;
-
+import java.io.*;
+import java.net.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 /**
  * Provides all methods necessary for a user application. This includes methods
  * for changing connectivity to the network (create, join, leave) as well as for
@@ -8,6 +12,10 @@ package chord;
  * @author 	Yu Sun
  */
 public class ChordNode {
+	/**
+	 * The identifier of this node
+	 */
+	public double identifier;
 	/**
 	 * The node's finger table. 
 	 */
@@ -21,5 +29,73 @@ public class ChordNode {
 	 */
 	protected ChordNode predecessor;
 	
+	private static final int MAX_RESPONSE_TIME = 5000;	//The maximum response time of this system.
+	private static final HashMap<Integer, String> clusterInfo = new HashMap<Integer, String>(); //Pid to every srever's process in the cluster.
+	protected List<Socket> socket = new LinkedList<Socket>(); 
+	protected static ServerSocket serverSocket = null;
+	public static void initConnection(String path, int maxNumOfSeates) throws IOException, FileNotFoundException {
+		
+		String serverInfo;
+		BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
+		while ((serverInfo = reader.readLine()) != null) {
+			//Split the serverInfo to get the host and port.
+			String[] splits = serverInfo.split(" ");
+			String ip = splits[0];
+			int port = Integer.parseInt(splits[1]);	
+			int hash = (ip + "/" + port).hashCode();
+			clusterInfo.put(hash, serverInfo);
+			
+		}
+		reader.close();
+		
+		//Get my pid.
+		System.out.println("My IP adress is: "+InetAddress.getLocalHost().getHostAddress());
+		//Write new node info into the file
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path)));
+		
+		//Add itself to the information sheet.
+		try{
+				//Specify a random port number.
+				int port = (int) (10000 + Math.random() * 10000);
+				String ip = InetAddress.getLocalHost().getHostAddress();
+				String data = port + ip + "\n";
+				writer.write(data);
+				writer.close();
+				
+				serverSocket = new ServerSocket(port);
+				while (true) {
+					//Wait for the client to connect.
+					Socket client = serverSocket.accept(); 
+					new ChordNodeThread(client); 
+					
+				}
+		}catch(IOException e){
+			System.out.println(e);
+		}
+		
+		try {
+			
+		} catch () {
+			
+		}
+		
+		if(serverSocket == null)
+				throw new IOException("Unable to find available port!");
+	}
+	
+	
+	public ChordNode find_successor(int id) {
+		
+	}
+	
+	public ChordNode find_predecessor(int id) {
+		
+	}
+	
+	public ChordNode closest_preceding_finger(int id) {
+		
+	}
+	
+
 	
 }
--- FinalProject/Chord/src/chord/ChordNodeThread.java
+++ FinalProject/Chord/src/chord/ChordNodeThread.java
@@ -1,5 +1,36 @@
 package chord;
+import java.io.*;
+import java.net.*;
 
-public class ChordNodeThread {
+public class ChordNodeThread extends Thread {
+	final ObjectInputStream istream; //The input stream
+	final ObjectOutputStream ostream; //The output stream
+	private Socket socket;
+	/**
+	 * Initialize a chord node thread with a socket input and output stream.
+	 * @param istream	//The input stream
+	 * @param ostream	//The output stream
+	 */
+	public ChordNodeThread(ObjectInputStream istream, ObjectOutputStream ostream) {
+		this.istream = istream;
+		this.ostream = ostream;
+	}
 
+	/**
+	 * Initialize a sever thread with a socket.
+	 * @param socket The socket.
+	 * @throws IOException If cannot initialize this thread due to an io error
+	 */
+	public ChordNodeThread(Socket socket) throws IOException{
+		this(new ObjectInputStream(socket.getInputStream()), new ObjectOutputStream(socket.getOutputStream()));
+	}
+	
+	@Override
+	public void run() {
+		try {
+			
+		} catch(IOException e) {
+			
+		}
+	}
 }
