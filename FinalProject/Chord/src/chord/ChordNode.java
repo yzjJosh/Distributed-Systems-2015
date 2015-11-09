1f30cdc7da015077acf8655c28cb823bb7ee0118
hidemon <yusun@utexas.edu>
2015-11-09 17:18:57 -0600

    Impementation

--- FinalProject/Chord/src/chord/ChordNode.java
+++ FinalProject/Chord/src/chord/ChordNode.java
@@ -1,4 +1,5 @@
 package chord;
+
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
@@ -8,115 +9,132 @@ import java.util.List;
 
 import communication.CommunicationManager;
 import communication.Message;
+import communication.MessageFilter;
 import communication.OnConnectionListener;
 import communication.OnMessageReceivedListener;
+
 /**
  * Provides all methods necessary for a user application. This includes methods
  * for changing connectivity to the network (create, join, leave) as well as for
  * working with content (insert, retrieve, remove).
  * 
- * @author 	Yu Sun
+ * @author Yu Sun
  */
-public class ChordNode  {
+public class ChordNode implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	/**
 	 * The identifier of this node
 	 */
 	ChordID identifier;
 	/**
-	 * The node's finger table. 
+	 * The node's finger table.
 	 */
 	protected FingerTable fingerTable;
 	/**
-	 * A reference to this node's successor. 
+	 * A reference to this node's successor.
 	 */
 	protected ChordNode successor;
 	/**
-	 * A reference to this node's predecessor. 
+	 * A reference to this node's predecessor.
 	 */
 	protected ChordNode predecessor;
-	
-	private static final int MAX_RESPONSE_TIME = 5000;	//The maximum response time of this system.
-	private static final HashMap<Integer, String> clusterInfo = new HashMap<Integer, String>(); //Pid to every srever's process in the cluster.
-	protected List<Socket> socket = new LinkedList<Socket>(); 
+
+	private static final int MAX_RESPONSE_TIME = 5000; // The maximum response time of this system.
+	private static final HashMap<Integer, String> clusterInfo = new HashMap<Integer, String>(); // information about other node.
+	private static final ArrayList<Integer> ids = new ArrayList<Integer>(); 
+															
+	protected List<Socket> socket = new LinkedList<Socket>();
 	protected static CommunicationManager manager = null;
-	
-	
-	public void initConnection(String path, int maxNumOfSeates) throws IOException, FileNotFoundException {
-		
+
+	public void initConnection(String path, int maxNumOfSeates)
+			throws IOException, FileNotFoundException {
+
 		String serverInfo;
-		BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
+		BufferedReader reader = new BufferedReader(new FileReader(
+				new File(path)));
 		while ((serverInfo = reader.readLine()) != null) {
-			//Split the serverInfo to get the host and port.
+			// Split the serverInfo to get the host and port.
 			String[] splits = serverInfo.split(" ");
 			String ip = splits[0];
-			int port = Integer.parseInt(splits[1]);	
+			int port = Integer.parseInt(splits[1]);
 			int hash = Integer.parseInt(splits[2]);
 			clusterInfo.put(hash, serverInfo);
-			
+			ids.add(hash);
+
 		}
 		reader.close();
+
+		// Get my pid.
+		System.out.println("My IP adress is: "
+				+ InetAddress.getLocalHost().getHostAddress());
+		// Write new node info into the file
+		BufferedWriter writer = new BufferedWriter(new FileWriter(
+				new File(path)));
+
+		// Add itself to the information sheet.
+
+		// Specify a random port number.
+		int port = (int) (10000 + Math.random() * 10000);
+		String ip = InetAddress.getLocalHost().getHostAddress();
+		identifier = new ChordID(port + ip);
+		String data = ip + " " + port + " " + identifier + "\n";
+		writer.write(data);
+		writer.close();
 		
-		//Get my pid.
-		System.out.println("My IP adress is: "+InetAddress.getLocalHost().getHostAddress());
-		//Write new node info into the file
-		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path)));
+	
+		int num = clusterInfo.size();
+		int index = (int) Math.random() * num;
+		String[] splits = clusterInfo.get(ids.get(index)).split(" ");
+		String connect_ip = splits[0];
+		int connect_port = Integer.parseInt(splits[1]);
+		// Connect to an exsiting node
+		int id_link = manager.connect(connect_ip, connect_port);
+		manager.setOnMessageReceivedListener(id_link, new OnMessageReceivedListener() {
+
+			@Override
+			public void OnMessageReceived(CommunicationManager manager, int id,
+					Message msg) {
 		
-		//Add itself to the information sheet.
-		try{
-				//Specify a random port number.
-				int port = (int) (10000 + Math.random() * 10000);
-				String ip = InetAddress.getLocalHost().getHostAddress();
-				identifier = new ChordID(port + ip);
-				String data = port + " " + ip + " " + identifier + "\n";
-				writer.write(data);
-				writer.close();
-				 
-				manager.waitForConnection(port, new OnConnectionListener() {
-
-					@Override
-					public void OnConnected(CommunicationManager manager, int id) {
-						manager.setOnMessageReceivedListener(id, new OnMessageReceivedListener() {
-
-							@Override
-							public void OnMessageReceived(
-									CommunicationManager manager, int id,
-									Message msg) {
-								 
-								
-							}
-
-							@Override
-							public void OnReceiveError(
-									CommunicationManager manager, int id) {
-								System.err.println("Receipt of Message Failed!");
-								
-							}
-							
-						});
-						
-					}
-
-					@Override
-					public void OnConnectFail(CommunicationManager manager) {
-						System.err.println("Connection dFailed!");
-						
-					}
-					
-				});
 				
-		}catch(IOException e){
-			System.out.println(e);
+			}
+
+			@Override
+			public void OnReceiveError(CommunicationManager manager, int id) {
+			
+				
+			}
+			
+		});
+		try {
+		manager.sendMessageForResponse(id_link, new Message().put("RequstJoin", "WantToJoin"), new MessageFilter() {
+
+			@Override
+			public boolean filter(Message msg) {
+				if (msg != null) {
+					if (msg.containsKey("Reply")) {
+						return true;
+					}
+				}
+				return false;
+			}		
+		}, MAX_RESPONSE_TIME,  new JoinMessageListener(this), false);
+		} catch (IOException e) {
+			System.err.println(e);
 		}
-		
+		manager.waitForConnection(port, new ChordNodeListener(this));
 
 	}
-	
+
 	/**
-	 * Find the successor node for the given identifier.  The successor
-	 * node is the node responsible for storing any <key, value> pair
-	 * whose key hashes to the identifier being sought.
+	 * Find the successor node for the given identifier. The successor node is
+	 * the node responsible for storing any <key, value> pair whose key hashes
+	 * to the identifier being sought.
 	 * 
-	 * @param id The identifier being sought
+	 * @param id
+	 *            The identifier being sought
 	 * 
 	 * @return The node responsible for id
 	 */
@@ -124,32 +142,32 @@ public class ChordNode  {
 		ChordNode m = find_predecessor(id);
 		return m.successor;
 	}
-	/*ask node to find id's precessor
-	 * 
+
+	/*
+	 * ask node to find id's precessor
 	 */
 	public ChordNode find_predecessor(ChordID id) {
 		ChordNode m = this;
-		while ( ! id.isBetween(this.getChordID(), successor.getChordID()) && ! id.equals(successor.getChordID())) {
+		while (!id.isBetween(this.getChordID(), successor.getChordID())
+				&& !id.equals(successor.getChordID())) {
 			m = m.closest_preceding_finger(id);
 		}
 		return m;
 	}
-	
 
-	
 	public ChordNode closest_preceding_finger(ChordID id) {
 
 		for (int i = 31; i >= 0; i--) {
 			FingerTableEntry finger = fingerTable.getFinger(i);
 			ChordID fingerID = finger.getNode().getChordID();
 			if (fingerID.isBetween(this.getChordID(), id)) {
-				return finger.getNode();			
+				return finger.getNode();
 			}
 		}
 		return this;
-		
+
 	}
-	
+
 	/**
 	 * Joins a Chord ring with a node in the Chord ring
 	 * 
@@ -160,7 +178,7 @@ public class ChordNode  {
 		predecessor = null;
 		successor = node.find_successor(this.getChordID());
 	}
-	
+
 	/**
 	 * Verifies the successor, and tells the successor about this node. Should
 	 * be called periodically.
@@ -195,23 +213,17 @@ public class ChordNode  {
 			finger.setNode(find_successor(key));
 		}
 	}
-	
+
 	public ChordID getChordID() {
 		return identifier;
 	}
-	
+
 	public ChordNode getPredecessor() {
 		return predecessor;
 	}
-	
+
 	public ChordNode getSuccessor() {
 		return successor;
 	}
-	
-	
 
-
-	
-
-	
 }
--- FinalProject/Chord/src/chord/ChordNodeListener.java
+++ FinalProject/Chord/src/chord/ChordNodeListener.java
@@ -1,5 +1,49 @@
 package chord;
 
-public class ChordNodeListener {
+import java.io.IOException;
+
+import communication.CommunicationManager;
+import communication.Message;
+import communication.OnConnectionListener;
+import communication.OnMessageReceivedListener;
+/**
+ * ChordNodeListener, can be seen as the server-end listener
+ * @author Yu Sun
+ */
+public class ChordNodeListener implements OnConnectionListener {
+	ChordNode node = null;
+	ChordNodeListener(ChordNode node) {
+		this.node = node;
+	}
+	@Override
+	public void OnConnected(CommunicationManager manager, int id) {
+		
+			manager.setOnMessageReceivedListener(id, new OnMessageReceivedListener() {
+
+				@Override
+				public void OnMessageReceived(CommunicationManager manager, int id, Message msg) {
+					if (msg.containsKey("RequestJoin")) {
+						try {
+							manager.sendMessage(id, new Message().put("Reply", node));
+						} catch (IOException e) {
+							e.printStackTrace();
+						}						
+					}
+				}
+
+				@Override
+				public void OnReceiveError(CommunicationManager manager, int id) {
+					System.err.println("Message Received Failed!");
+				}
+				
+			});
+			System.out.println("Link " + id + " is connected!!!");	
+	}
+
+	@Override
+	public void OnConnectFail(CommunicationManager manager) {
+		System.err.println("Connection dFailed!");
+		
+	}
 
 }
