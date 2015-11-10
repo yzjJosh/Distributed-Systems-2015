88b6da24ae7fc3bce48c7a3e5f384f0af0f05c55
hidemon <yusun@utexas.edu>
2015-11-09 20:38:32 -0600

    Implementation+=+

--- FinalProject/Chord/src/chord/FingerTable.java
+++ FinalProject/Chord/src/chord/FingerTable.java
@@ -1,5 +1,6 @@
 package chord;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,26 +9,46 @@ import java.util.List;
  *  * @author 	Yu Sun
  */
 
-public class FingerTable {
-		List <FingerTableEntry> fingers = new ArrayList<FingerTableEntry> ();
-		int size = 0;
-		/**
-		 * Add a new entry into the finger table of node n.
-		 * 
-		 * @param start: The identity of the first node, s, that succeeds n by at least 2^(i-1) on the identifier circle.
-		 * @param interval_0: finger[k].start
-		 * @param interval_ 1: finger[k+1].start
-		 * @param successor: the previous node on the identifier circle
-		 */
-		public void addNewEntry(ChordID start, ChordID interval_0, ChordID interval_1,  ChordNode node) {
-			FingerTableEntry entry = new FingerTableEntry(start, interval_0, interval_1, node);
-			fingers.add(entry);
-			size++;
+public class FingerTable implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	FingerTableEntry[] fingers;
+
+	public FingerTable(ChordNode node) {
+		this.fingers = new FingerTableEntry[32];
+		for (int i = 0; i < fingers.length; i++) {
+			ChordID start = node.getChordID();
+			fingers[i] = new FingerTableEntry(start, node);
 		}
-		
-		public FingerTableEntry getFinger(int index) {
-			return fingers.get(index);
-		}
+	}
+
+	public FingerTableEntry getFinger(int i) {
+		return fingers[i];
+	}
+	
+	
+//		List <FingerTableEntry> fingers = new ArrayList<FingerTableEntry> ();
+//		int size = 0;
+//		/**
+//		 * Add a new entry into the finger table of node n.
+//		 * 
+//		 * @param start: The identity of the first node, s, that succeeds n by at least 2^(i-1) on the identifier circle.
+//		 * @param interval_0: finger[k].start
+//		 * @param interval_ 1: finger[k+1].start
+//		 * @param successor: the previous node on the identifier circle
+//		 */
+//		public void addNewEntry(ChordID start, ChordID interval_0, ChordID interval_1,  ChordNode node) {
+//			FingerTableEntry entry = new FingerTableEntry(start, interval_0, interval_1, node);
+//			fingers.add(entry);
+//			size++;
+//		}
+//		
+//		public FingerTableEntry getFinger(int index) {
+//			return fingers.get(index);
+//		}
 		
 		
 }
